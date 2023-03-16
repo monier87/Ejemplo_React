06021a6473089c031ba9d4e67f76b78eb48df305
monier87 <carlos.monier87@gmail.com>
2023-03-16 10:28:47 -0400

    Cambios 16MARZ

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,13 +1,21 @@
-
 import React from 'react';
 
 function Testimonio() {
-    return(
-        <div className='contenedor-testimonio' >
-            <img 
-                className='imagen-testimonio'
-             />
+    return (
 
+      <div className='contenedor-testimonio'>
+        <img 
+          className='imagen-testimonio'
+          src={require('../imagenes/testimonio-emma.png')}
+          alt='Foto de Emma'/>
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>Emma Bostia en Suecia</p>
+          <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
+          <p className='Texto-testimonio'>ESto es un parrofo de Prueba.kshfkasd,kbfad,msbfmnbsdf,mb,kdasbf,mdasb,mfdsba,mbdsaf,mdsa,mbdfas,mbfd,masd,msbd,ms</p>
         </div>
+      </div>
+
     );
 }
+
+export default Testimonio;
